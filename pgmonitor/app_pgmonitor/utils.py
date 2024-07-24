28760ad7cc91179f0a92000c8cc10c5fb143f5b1
Adrian Jaquier <adrianwilmer@hotmail.com>
2024-07-24 09:21:33 -0300

    Funcionalidade de expandir tamanho das tabelas

--- pgmonitor/app_pgmonitor/utils.py
+++ pgmonitor/app_pgmonitor/utils.py
@@ -24,6 +24,24 @@ def get_usuarios_total():
         result = cursor.fetchone()
     return result[0] if result else 0
 
+def get_base_tamanho_tabelas():
+    with connections['default'].cursor() as cursor:
+        query = """
+        SELECT esquema, tabela,
+       pg_size_pretty(pg_relation_size(esq_tab)) AS tamanho,
+       pg_size_pretty(pg_total_relation_size(esq_tab)) AS tamanho_total
+  FROM (SELECT tablename AS tabela,
+               schemaname AS esquema,
+               schemaname||'.'||tablename AS esq_tab
+          FROM pg_catalog.pg_tables
+         WHERE schemaname NOT
+            IN ('pg_catalog', 'information_schema', 'pg_toast') ) AS x
+ ORDER BY pg_total_relation_size(esq_tab) DESC;
+        """
+        cursor.execute(query)
+        result = cursor.fetchall()
+    return result
+
 def get_base_tamanho():
     with connections['default'].cursor() as cursor:
         query = """
--- pgmonitor/app_pgmonitor/views.py
+++ pgmonitor/app_pgmonitor/views.py
@@ -8,9 +8,11 @@ def home(request):
     logged_in_users = get_usuarios()
     usuarios_total = get_usuarios_total()
     tamanho_base = get_base_tamanho()
+    tamanho_base_tabelas = get_base_tamanho_tabelas()
     #RENDER
     return render(request, 'monitor/home.html', 
                   {'logged_in_users': logged_in_users, 
                    'usuarios_total': usuarios_total,
+                   'tamanho_base_tabelas': tamanho_base_tabelas,
                    'tamanho_base': tamanho_base,
                    'base_1': constants.BASE_1})
